ae5f309ac82011b9e6d60770faf91a59a576a438
BENDBIM <jssbim001@gmail.com>
2022-05-17 23:26:52 +0545

    Terrace Farming Project

---
+++ store/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+	
+
+	path('', views.store, name="store"),
+	path('cart/', views.cart, name="cart"),
+	path('checkout/', views.checkout, name="checkout"),
+
+	path('update_item/', views.updateItem, name="update_item"),
+
+	path('register/', views.register, name="register"),
+	path('login/', views.login, name="login"),  
+	path('logout/', views.logout, name="logout"),
+]
